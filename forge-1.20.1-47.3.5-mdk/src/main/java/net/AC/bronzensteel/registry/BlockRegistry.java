f9b0033fbbc882c763cd0517e468105ef5e71c1e
All-Chronical <a.c.a11.creator@gmail.com>
2024-08-02 14:26:38 -0400

    Root name and ID Change from bronzeandsteel/Bronze And Steel to bronzensteel/Bronze N' Steel

---
+++ forge-1.20.1-47.3.5-mdk/src/main/java/net/AC/bronzensteel/registry/BlockRegistry.java
@@ -0,0 +1,39 @@
+package net.AC.bronzensteel.registry;
+
+import net.AC.bronzensteel.bronzensteel;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class BlockRegistry {
+
+    // Initializing Deferred Register for BLOCKS
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, bronzensteel.MOD_ID);
+
+
+    // Registering Material Blocks
+    public static final RegistryObject<Block> STEEL_BLOCK = BLOCKS.register
+            ("steel_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)
+                    .mapColor(MapColor.COLOR_GRAY)
+                    .explosionResistance(10.0f)
+                    ));
+
+    public static final RegistryObject<DropExperienceBlock> TIN_ORE = BLOCKS.register
+            ("tin_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.COAL_ORE)
+            ));
+
+    // Eventbus handler
+    public static void register(IEventBus bus){
+        BLOCKS.register(bus);
+    }
+}
---
+++ forge-1.20.1-47.3.5-mdk/src/main/java/net/AC/bronzensteel/registry/CreativeModeTabs.java
@@ -0,0 +1,44 @@
+package net.AC.bronzensteel.registry;
+
+import net.AC.bronzensteel.bronzensteel;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.rmi.registry.Registry;
+
+public class CreativeModeTabs {
+
+    // Initializing Deferred Register for CREATIVE_MODE_TABS
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, bronzensteel.MOD_ID);
+
+    // Registering BNS_CREATIVE_MENU
+    public static final RegistryObject<CreativeModeTab> BNS_CREATIVE_MENU = CREATIVE_MODE_TABS.register("bns_creative_menu",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ItemRegistry.STEEL_INGOT.get()))
+
+                    // Translation Key
+                    .title(Component.translatable("creativetab.creative_menu"))
+
+                    // Content
+                    .displayItems(((itemDisplayParameters, output) ->
+                    {
+                        output.accept(ItemRegistry.TIN_ORE__ITEM.get());
+                        output.accept(ItemRegistry.STEEL_INGOT.get());
+                        output.accept(ItemRegistry.STEEL_BLOCK__ITEM.get());
+                    }))
+
+                    .build());
+
+    // Eventbus handler
+    public static void register(IEventBus bus){
+        CREATIVE_MODE_TABS.register(bus);
+    }
+}
+
